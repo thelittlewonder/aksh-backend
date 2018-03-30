e76236221d13d63f856d495220a50d8c5ee7fe0b
Himanshu Shekhar <himanshushekharb16@gmail.com>
2018-03-31 01:15:19 +0530

    add random schools and hospitals

--- populate-database.js
+++ populate-database.js
@@ -3,119 +3,160 @@ const Papa = require("papaparse");
 const MongoClient = require("mongodb").MongoClient;
 
 function removeHiddenFiles(filelist) {
-  return filelist.filter(f => !f.startsWith("."));
+    return filelist.filter(f => !f.startsWith("."));
 }
 
 function readAllCSV(datadir) {
-  let files = fs.readdirSync(datadir);
-  files = removeHiddenFiles(files);
-  let csvfiles = files.filter(f => f.endsWith("csv"));
-  let keys = csvfiles.map(f => f.split("_")[1].split(".")[0]);
+    let files = fs.readdirSync(datadir);
+    files = removeHiddenFiles(files);
+    let csvfiles = files.filter(f => f.endsWith("csv"));
+    let keys = csvfiles.map(f => f.split("_")[1].split(".")[0]);
 
-  let data = {};
-  for (let i = 0; i < keys.length; i++) {
-    // trim required because files have trailing whitespace
-    let content = fs
-      .readFileSync(`${datadir}/${csvfiles[i]}`)
-      .toString()
-      .trim();
-    data[keys[i]] = Papa.parse(content, { header: true }).data;
-  }
-  return data;
+    let data = {};
+    for (let i = 0; i < keys.length; i++) {
+        // trim required because files have trailing whitespace
+        let content = fs
+        .readFileSync(`${datadir}/${csvfiles[i]}`)
+        .toString()
+        .trim();
+        data[keys[i]] = Papa.parse(content, { header: true }).data;
+    }
+    return data;
 }
 
 function readAllGeoJson(datadir) {
-  let files = fs.readdirSync(datadir);
-  files = removeHiddenFiles(files);
-  let geojsonfiles = files.filter(f => f.endsWith("json"));
-  let keys = geojsonfiles.map(f => f.split("_")[1].split(".")[0]);
+    let files = fs.readdirSync(datadir);
+    files = removeHiddenFiles(files);
+    let geojsonfiles = files.filter(f => f.endsWith("json"));
+    let keys = geojsonfiles.map(f => f.split("_")[1].split(".")[0]);
 
-  let data = {};
-  for (let i = 0; i < keys.length; i++) {
-    data[keys[i]] = require(`${datadir}/${geojsonfiles[i]}`);
-  }
-  return data;
+    let data = {};
+    for (let i = 0; i < keys.length; i++) {
+        data[keys[i]] = require(`${datadir}/${geojsonfiles[i]}`);
+    }
+    return data;
 }
 
 function calculateCentroid(coordinates) {
-  coordinates = coordinates[0]
-  let centroid_x = 0.0
-  let centroid_y = 0.0
-  for (let i of coordinates){
-      centroid_x += i[0]
-      centroid_y += i[1]
-  }
-  centroid = [centroid_x/coordinates.length, centroid_y/coordinates.length]
-  
-  return  {"type": "Point", "coordinates":centroid}
-  
-}
-function combineCSVandGeoJson(allCSV, allGeoJson) {
-  let combinedData = {};
-
-  for (let key of Object.keys(allCSV)) {
-    // each key goes to each collection in Mongo
-    // entries are individual records in a collection
-    let entries = [];
-
-    for (let i = 0; i < allCSV[key].length; i++) {
-      entries.push({
-        location: allGeoJson[key].geometries[i],
-        centroid: calculateCentroid(allGeoJson[key].geometries[i].coordinates),
-        metadata: allCSV[key][i]
-      });
+    coordinates = coordinates[0];
+    let centroid_x = 0.0;
+    let centroid_y = 0.0;
+    for (let i of coordinates){
+        centroid_x += i[0];
+        centroid_y += i[1];
     }
-    combinedData[key] = entries;
-  }
-  return combinedData;
+    centroid = [centroid_x/coordinates.length, centroid_y/coordinates.length];
+
+    return  {"type": "Point", "coordinates":centroid};
+}
+
+function combineCSVandGeoJson(allCSV, allGeoJson) {
+    let combinedData = {};
+
+    for (let key of Object.keys(allCSV)) {
+        // each key goes to each collection in Mongo
+        // entries are individual records in a collection
+        let entries = [];
+
+        for (let i = 0; i < allCSV[key].length; i++) {
+            entries.push({
+                location: allGeoJson[key].geometries[i],
+                centroid: calculateCentroid(allGeoJson[key].geometries[i].coordinates),
+                metadata: allCSV[key][i]
+            });
+        }
+        combinedData[key] = entries;
+    }
+    return combinedData;
 }
 
 async function insertCombinedDataToMongo(combinedData, dbUrl, dbName) {
-  try {
-    var conn = await MongoClient.connect(dbUrl);
-    var db = conn.db(dbName);
-  } catch (e) {
-    console.log("error connecting", e);
-    return;
-  }
+    try {
+        var conn = await MongoClient.connect(dbUrl);
+        var db = conn.db(dbName);
+    } catch (e) {
+        console.log("error connecting", e);
+        return;
+    }
 
-  for (let coll of Object.keys(combinedData)) {
-    // console.log('Inserting', combinedData[coll].length, 'items to', coll)
-    process.stdout.write(`Dropping collection ${coll} ... `);
-    await db.dropCollection(coll)
-    process.stdout.write(" DONE \n");
+    for (let coll of Object.keys(combinedData)) {
+        // console.log('Inserting', combinedData[coll].length, 'items to', coll)
+        try {
+            process.stdout.write(`Dropping collection ${coll} ... `);
+            await db.dropCollection(coll);
+            process.stdout.write(" DONE \n");
+        } catch (e) {}
 
-    process.stdout.write(
-      `Inserting ${combinedData[coll].length} items to ${coll} ... `
-    );
-    await db.collection(coll).insertMany(combinedData[coll]);
-    process.stdout.write(" DONE \n");
-    // console.log('DONE')
-  }
+        process.stdout.write(
+            `Inserting ${combinedData[coll].length} items to ${coll} ... `
+        );
+        await db.collection(coll).insertMany(combinedData[coll]);
+        process.stdout.write(" DONE \n");
+        // console.log('DONE')
+    }
 
-  await conn.close();
-  console.log("All entries made successfully");
+    await conn.close();
+    console.log("All entries made successfully");
+}
+
+async function markRandomSchoolsAndHospitals(lulcData, dbUrl, dbName) {
+    let nSchools = (Math.abs(0.5 - Math.random()) * 100) % lulcData.length;
+    let nHospitals = (Math.abs(0.5 - Math.random()) * 100) % lulcData.length;
+
+    let villages = lulcData.filter(x => (x['metadata']['lc_code'] == 'BURV') || (x['metadata']['lc_code'] == 'BURH'));
+
+    villages.sort(function() { return Math.random() });   // shuffle
+    let schoolsHere = villages.slice(0, nSchools);
+    for (let each of schoolsHere) {
+        each.location = each.centroid;
+    }
+
+    villages.sort(function() { return Math.random() });   // shuffle
+    let hospitalsHere = villages.slice(0, nHospitals);
+    for (let each of hospitalsHere) {
+        each.location = each.centroid;
+    }
+
+    try {
+        var conn = await MongoClient.connect(dbUrl);
+        var db = conn.db(dbName);
+    } catch (e) {
+        console.log("error connecting", e);
+        return;
+    }
+
+    try {
+        await db.dropCollection('School');
+        await db.dropCollection('Hospital');
+    } catch (e) {}
+
+    await db.collection('School').insertMany(schoolsHere);
+    await db.collection('Hospital').insertMany(hospitalsHere);
+
+    conn.close();
 }
 
 module.exports = {
-  readAllCSV,
-  readAllGeoJson,
-  combineCSVandGeoJson,
-  insertCombinedDataToMongo
+    readAllCSV,
+    readAllGeoJson,
+    combineCSVandGeoJson,
+    insertCombinedDataToMongo
 };
 
 if (require.main === module) {
-  // console.log(readAllCSV('./dataset/Kurudu-csv'))
-  // console.log(readAllGeoJson('./dataset/Kurudu-geojson'))
+    // console.log(readAllCSV('./dataset/Kurudu-csv'))
+    // console.log(readAllGeoJson('./dataset/Kurudu-geojson'))
 
-  let allCSV = readAllCSV("./dataset/Kurudu-csv");
-  let allGeoJson = readAllGeoJson("./dataset/Kurudu-geojson");
-  
-  let combinedData = combineCSVandGeoJson(allCSV, allGeoJson);
-  console.log(combinedData)
-  console.log(combinedData["LULC"][0]);
+    let allCSV = readAllCSV("../dataset/Kurudu-csv");
+    let allGeoJson = readAllGeoJson("../dataset/Kurudu-geojson");
 
-  let dbUrl = "mongodb://localhost:27017/";
-  let dbName = "sih";
-  insertCombinedDataToMongo(combinedData, dbUrl, dbName);
+    let combinedData = combineCSVandGeoJson(allCSV, allGeoJson);
+    // console.log(combinedData);
+    console.log(combinedData["LULC"][0]);
+
+    let dbUrl = "mongodb://localhost:27017/";
+    let dbName = "sih";
+    insertCombinedDataToMongo(combinedData, dbUrl, dbName);
+
+    markRandomSchoolsAndHospitals(combinedData['LULC'], dbUrl, dbName):
 }
